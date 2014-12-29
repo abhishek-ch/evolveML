11b02a719460c6917d112a340b10c9107b69fa4f
Abhishek Choudhary <abhishek.create@gmail.com>
2014-12-29 19:59:08 +0000

    Spark Proper Working Examples

---
+++ test/Spark.py
@@ -0,0 +1,23 @@
+__author__ = 'abhishekchoudhary'
+
+import os
+import pandas as pd
+from pyspark.mllib.feature import HashingTF, IDF, Vectors
+from pyspark.mllib.classification import NaiveBayes
+from pyspark.mllib.regression import LabeledPoint
+from pyspark import SparkContext, SparkConf
+from nltk.corpus import stopwords
+
+
+def parseCSV(line):
+    print "-----------------------------------------"
+    values = [s for s in line.strip().split(",")]
+    print "1 ====== %s AND 2 =>>>>>%s" % (values[0], values[3])
+
+
+# conf = (SparkConf().setMaster("yarn-client").setAppName("My Long Data Set").set("spark.executor.memory", "1g"))
+sc = SparkContext("local[8]", appName="Test CSV")
+csv = sc.textFile("hdfs:///stats/testdata.manual.2009.06.14.csv").map(parseCSV)
+print("================>>>>>>>>>>>  ",csv.collect())
+# csv = csv.map(parseCSV)
+sc.stop()
