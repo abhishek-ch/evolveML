260c24f5ece8f0f32f6f122ac02a4320ad6f8088
ABHISHEK CHOUDHARY <abhishek.create@gmail.com>
2015-01-08 17:07:07 +0000

    feature extraction 1st sample

---
+++ test/FeatureFactory.py
@@ -0,0 +1,235 @@
+import json, sys
+import base64
+from Datum import Datum
+import string
+import re
+
+#https://github.com/alexsmirnov/nlp-class-assignments/blob/master/pa4-ner/python/FeatureFactory.py
+
+class FeatureFactory:
+    """
+    Add any necessary initialization steps for your features here
+    Using this constructor is optional. Depending on your
+    features, you may not need to intialize anything.
+    """
+
+    def __init__(self):
+        self._digits = re.compile('\d\.')
+        self.mister = frozenset(['mr',"ms","ms.","mss","mr.","dr","dr.","de","fon"])
+        self.midname = re.compile("^[A-Z]\.$")
+
+
+    """
+    Words is a list of the words in the entire corpus, previousLabel is the label
+    for position-1 (or O if it's the start of a new sentence), and position
+    is the word you are adding features for. PreviousLabel must be the
+    only label that is visible to this method. 
+    """
+
+    def computeFeatures(self, words, previousLabel, position):
+        features = []
+
+        currentWord = words[position]
+        currentCapitalized = currentWord[0].isupper() and currentWord[1:].islower()
+        previousWord = words[position - 1] or "=NOT-A-WORD="
+
+        """ Baseline Features """
+        features.append("word=" + currentWord)
+        features.append("prevLabel=" + previousLabel)
+        features.append("word=" + currentWord + ", prevLabel=" + previousLabel)
+
+        # if currentWord[0].isupper() and not currentWord.isupper():
+        # features.append("case=Title")
+        '''
+            sort out punctuation before any capital word
+        '''
+
+        '''
+        for j in previousWord:
+            if j in string.punctuation:
+                if (currentWord[0].isupper() or currentWord.isupper()):
+                    features.append("Punctuation")
+                    #print(currentWord)
+                    break;
+        '''
+
+        '''
+        if bool(self._digits.search(previousWord)) and currentWord[0].isupper():
+            features.append("PrevNum")
+        if currentWord[0].isupper() and not currentWord.isupper():
+            features.append("case=Title")
+        if previousWord in string.punctuation and (currentWord[0].isupper() or currentWord.isupper()):
+            features.append("Punctuation")
+            #print('ALLA KASAMMMMM',currentWord)
+        if currentWord == 'Washington':
+            features.append('specific')
+
+        '''
+
+        """
+            Warning: If you encounter "line search failure" error when
+            running the program, considering putting the baseline features
+        back. It occurs when the features are too sparse. Once you have
+            added enough features, take out the features that you don't need.
+        """
+
+        """ TODO: Add your features here """
+
+        if previousWord.lower() in self.mister:
+            features.append("mister=" + previousWord.lower())
+        if self.midname.match(previousWord) and currentCapitalized:
+            features.append("afterMidName")
+        if self.midname.match(currentWord) and previousLabel == "PERSON":
+            features.append("MidName")
+        if previousWord.lower() == 'the':
+            features.append("afterArticle")
+        if previousLabel == "PERSON" and currentCapitalized:
+            features.append("afterperson")
+        if currentWord.endswith("'s"):
+            features.append("owns")
+        if currentWord.startswith("O'"):
+            features.append("ireland")
+        if currentWord.startswith("D'"):
+            features.append("french")
+        if currentCapitalized:
+            features.append("capitalized")
+
+
+        return features
+
+
+    """ Do not modify this method """
+
+
+    def readData(self, filename):
+        data = []
+
+        for line in open(filename, 'r'):
+            line_split = line.split()
+            # remove emtpy lines
+            if len(line_split) < 2:
+                continue
+            word = line_split[0]
+            label = line_split[1]
+
+            datum = Datum(word, label)
+            data.append(datum)
+
+        return data
+
+
+    """ Do not modify this method """
+
+
+    def readTestData(self, ch_aux):
+        data = []
+
+        for line in ch_aux.splitlines():
+            line_split = line.split()
+            # remove emtpy lines
+            if len(line_split) < 2:
+                continue
+            word = line_split[0]
+            label = line_split[1]
+
+            datum = Datum(word, label)
+            data.append(datum)
+
+        return data
+
+
+    """ Do not modify this method """
+
+
+    def setFeaturesTrain(self, data):
+        newData = []
+        words = []
+
+        for datum in data:
+            words.append(datum.word)
+
+        # # This is so that the feature factory code doesn't
+        ## accidentally use the true label info
+        previousLabel = "O"
+        for i in range(0, len(data)):
+            datum = data[i]
+
+            newDatum = Datum(datum.word, datum.label)
+            newDatum.features = self.computeFeatures(words, previousLabel, i)
+            newDatum.previousLabel = previousLabel
+            newData.append(newDatum)
+
+            previousLabel = datum.label
+
+        return newData
+
+
+    """
+    Compute the features for all possible previous labels
+    for Viterbi algorithm. Do not modify this method
+    """
+
+
+    def setFeaturesTest(self, data):
+        newData = []
+        words = []
+        labels = []
+        labelIndex = {}
+
+        for datum in data:
+            words.append(datum.word)
+            if not labelIndex.has_key(datum.label):
+                labelIndex[datum.label] = len(labels)
+                labels.append(datum.label)
+
+        # # This is so that the feature factory code doesn't
+        ## accidentally use the true label info
+        for i in range(0, len(data)):
+            datum = data[i]
+
+            if i == 0:
+                previousLabel = "O"
+                datum.features = self.computeFeatures(words, previousLabel, i)
+
+                newDatum = Datum(datum.word, datum.label)
+                newDatum.features = self.computeFeatures(words, previousLabel, i)
+                newDatum.previousLabel = previousLabel
+                newData.append(newDatum)
+            else:
+                for previousLabel in labels:
+                    datum.features = self.computeFeatures(words, previousLabel, i)
+
+                    newDatum = Datum(datum.word, datum.label)
+                    newDatum.features = self.computeFeatures(words, previousLabel, i)
+                    newDatum.previousLabel = previousLabel
+                    newData.append(newDatum)
+
+        return newData
+
+
+    """
+    write words, labels, and features into a json file
+    Do not modify this method
+    """
+
+
+    def writeData(self, data, filename):
+        outFile = open(filename + '.json', 'w')
+        for i in range(0, len(data)):
+            datum = data[i]
+            jsonObj = {}
+            jsonObj['_label'] = datum.label
+            jsonObj['_word'] = base64.b64encode(datum.word)
+            jsonObj['_prevLabel'] = datum.previousLabel
+
+            featureObj = {}
+            features = datum.features
+            for j in range(0, len(features)):
+                feature = features[j]
+                featureObj['_' + feature] = feature
+            jsonObj['_features'] = featureObj
+
+            outFile.write(json.dumps(jsonObj) + '\n')
+
+        outFile.close()
+
