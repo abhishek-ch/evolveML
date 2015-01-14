368c1f1f5eed878bd92773119072638dc9099025
Abhishek Choudhary <abhishek.create@gmail.com>
2015-01-14 23:00:08 +0000

    NamedValueTuple update and refactored the data reading from tsp

--- kaggle/KaggleMovieSentAnalysis.py
+++ kaggle/KaggleMovieSentAnalysis.py
@@ -2,12 +2,13 @@ __author__ = 'achoudhary'
 
 import os
 import csv
-from MovieRecord import MovieSingleData
+from MovieRecord import MovieSingleData,MovieData
 
 class DataReader(object):
     #http://stackoverflow.com/questions/36901/what-does-double-star-and-star-do-for-python-parameters
     #https://github.com/rafacarrascosa/samr/blob/develop/samr/corpus.py
-    def readFilelinebyline(self,filepath):
+    def readFilelinebyline(self,DIRECTORY):
+        filepath = os.path.join(DIRECTORY, 'train.tsv')
         if os.path.exists(filepath):
             print 'Awesome'
             #read the file contents seperated by tab with ignoring the first row
@@ -16,17 +17,27 @@ class DataReader(object):
             row = next(iter) #ignore first row
 
             for row in iter:
-                #print 'length %s row %s '%(len(row),row[0])
-                #yield MovieData(*row) #* passes the value as list
-                yield MovieSingleData(row[0],row[1],row[2],row[3])
+                yield MovieData(*row)   #* passes the value as list
+                #yield MovieSingleData(row[0],row[1],row[2],row[3])
 
         else:
             print 'Sorry, but file path is invalid...'
 
+    def getData(self,datalist=[]):
+        #get current project root -> one step up -> get data dir -> get the file
+        BASE_DATA_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..', 'data'))
+        datalist.extend(self.readFilelinebyline(BASE_DATA_PATH))
+        return datalist
 
+
+    def getTrainTestData(self):
+        reader = DataReader()
+        data = list(reader.getData())
+        return data
 
 
 if __name__ == '__main__':
     reader = DataReader()
-    cached = []
-    cached.extend(reader.readFilelinebyline('C:\\Users\\achoudhary\\Downloads\\train.tsv\\train.tsv'))
+    data = reader.getTrainTestData()
+    for val in data:
+        print(val.Phrase)
