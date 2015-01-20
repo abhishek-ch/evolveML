4754c3e251fae65cdedd0d0f0f8fadb60e9bfbd7
Abhishek Choudhary <abhishek.create@gmail.com>
2015-01-20 00:12:44 +0000

    improved gaggle with onevsone classifier

--- kaggle/KaggleMovieSentAnalysis.py
+++ kaggle/KaggleMovieSentAnalysis.py
@@ -2,7 +2,7 @@ __author__ = 'achoudhary'
 
 import os
 import csv
-from MovieRecord import  MovieData,TestMovieData
+from MovieRecord import MovieData, TestMovieData
 import random
 import nltk
 import collections
@@ -14,7 +14,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
-from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multiclass import OneVsRestClassifier,OneVsOneClassifier
 
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
@@ -23,13 +23,14 @@ from sklearn.cross_validation import cross_val_score
 from sklearn import cross_validation
 from sklearn.svm import SVC
 import numpy as np
+import pandas as pd
 from sklearn.cross_validation import KFold
 
-
+#todo https://github.com/yogesh-kamble/kaggle-submission/blob/master/movie_review.py
 class DataReader(object):
     def __init__(self):
         self.signature = ['Mr.', 'Mrs.', 'Dr.', 'Ms.', 'Miss.']
-        #self.stop_words = list(set(stopwords.words('english')))
+        # self.stop_words = list(set(stopwords.words('english')))
         self.remove_words = ["'ve", "'nt", "'ll", "n't", '...', "'re'", "'s"]
         self.previouswords = ['was', 'do', 'have', 'were', 'had', 'need', 'has', 'did']
         self.pattern1 = re.compile("^[.-]+\w+[.-]+$")
@@ -40,14 +41,19 @@ class DataReader(object):
 
         self.sents = {'0': 'neg', '1': 'someneg', '2': 'neutral', '3': 'sompos', '4': 'pos'}
 
-        self.matchingparam = [',','.']
+        self.matchingparam = [',', '.']
         self.allPhrases = []
-        self.stop_words=['in','a','an','i','are','for','oh','hi','all','on','am','is','once','it','we','be']
-        # self.most_significant_words = []
+        # self.stop_words = ['this', 'a', 'of', 'to', 'at', 'movie', 'film',  'in',
+        # 'it', 'as', 'an', 'be', 'on',  'is', 'or', 'for','so'
+        #                   ]
+        self.stop_words = ['the', 'a', 'of', 'and', 'to', 'in', 'is', 'that', 'it', 'as', 'with', 'for', 'its',
+                           'an', 'of the', 'film', 'this', 'movie', 'be', 'on', 'all', 'by', 'or', 'at', 'not', 'like'
+            , 'you',  'more', 'his', 'are', 'has', 'so', "``"]
+        # self.most_significant_words = [],
 
     # http://stackoverflow.com/questions/36901/what-does-double-star-and-star-do-for-python-parameters
     # https://github.com/rafacarrascosa/samr/blob/develop/samr/corpus.py
-    def readFilelinebyline(self, DIRECTORY,file='train.tsv',movieRecordType=MovieData):
+    def readFilelinebyline(self, DIRECTORY, file='train.tsv', movieRecordType=MovieData):
         filepath = os.path.join(DIRECTORY, file)
         if os.path.exists(filepath):
             # read the file contents seperated by tab with ignoring the first row
@@ -86,152 +92,60 @@ class DataReader(object):
         datalist.extend(self.readFilelinebyline(BASE_DATA_PATH))
 
         testdata = []
-        testdata.extend(self.readFilelinebyline(BASE_DATA_PATH,'test.tsv',TestMovieData))
+        testdata.extend(self.readFilelinebyline(BASE_DATA_PATH, 'test.tsv', TestMovieData))
 
-        return datalist,testdata
-
-
-    def cleanData(self, data=[]):
-        specific_words_dict = collections.defaultdict(list)
-        all_words = []
-        skip = False
-        if len(data) < 2:
-            print 'Sorry but its too less data for ML'
-        else:
-            for line in data:
-                phrase = line.Phrase
-                sentiment = line.Sentiment
-                i = 0
-                _cached_ = []
-
-                for word in phrase.split():
-                    if skip:
-                        skip = False
-                        continue
-                    if word in self.signature:
-                        # print('Sig ',word)
-                        skip = True
-                        continue
-                    if len(word) > 0 and (word[0].isupper() and i > 0) or word.isupper():
-                        # print('Upper ',word)
-                        continue
-                    if self.worddashword.match(word.lower()) or word in string.punctuation or word[
-                        0] in string.punctuation:
-                        # print('WOD SSAHAHHAHAHAH DASHHH ',word)
-                        continue
-                    if word.lower() in self.stop_words:
-                        # print('Stop ',word)
-                        continue
-                    if word.lower() in self.remove_words:
-                        # print('remo ',word)
-                        continue
-                    if self.pattern1.match(word.lower()):
-                        # print('Pattern ',word)
-                        continue
-                    if self.useless.match(word.lower()):
-                        continue
-                    # print('FINANA ',word)
-                    all_words.append(word.lower())
-                    _cached_.append(word.lower())
-                    # For First Worst Check
-                    i += 1
-                line = ' '.join(_cached_)
-                specific_words_dict[sentiment].append(line)
-        return specific_words_dict, all_words
+        return datalist, testdata
 
 
     def tokenize_data(self, phrase):
-        i = -1
         _cached_ = []
-        skip = False
 
         for word in phrase.split():
-            i += 1
-            if skip:
-                skip = False
-                continue
             if word in self.signature:
                 # print('Sig ',word)
-                skip = True
                 continue
-            if len(word) > 0 and (word[0].isupper() and i > 0) or word.isupper():
+            if len(word) > 0 and (word[0].isupper() and len(_cached_) > 1) or word.isupper():
                 # print('Upper ',word)
                 continue
-            if self.worddashword.match(word.lower()) or word in string.punctuation or word[0] in string.punctuation:
-                # print('WOD SSAHAHHAHAHAH DASHHH ',word)
+            if self.worddashword.match(word.lower()):
                 continue
             if word.lower() in self.stop_words:
-                # print('Stop ',word)
-             continue
+                continue
             if word.lower() in self.remove_words:
-                # print('remo ',word)
+                if len(_cached_) > 0:
+                    _cached_.pop()
                 continue
             if self.pattern1.match(word.lower()):
                 # print('Pattern ',word)
                 continue
             if self.useless.match(word.lower()):
                 continue
-            # print('FINANA ',word)
+            if word in string.punctuation:
+                continue
+
             _cached_.append(word.lower())
             # For First Worst Check
 
-
-        self.allPhrases.append(phrase)
-
         return _cached_
-
-    # find the frequency of the all word list
-    def wordFrequency(self, data=[]):
-        if len(data) < 2:
-            return ['Sorry but its too less data for ML']
-        else:
-            specific_words_dict, all_words = self.cleanData(data)
-            word_features = nltk.FreqDist(all_words)
-            print(word_features.most_common(30))
-            return specific_words_dict, all_words, word_features.keys()[:9000]
-
-
-    # extracting features from the dataset extracted
-    # it currently checks the availability among the most popular words in the list
-    def feature_extraction(self, specific_words_dict):
-        features_X = []
-        features_Y = []
-        for sentiment, values in specific_words_dict.iteritems():
-            for line in values:
-                features_X.append(line)
-                features_Y.append(self.sents[sentiment])
-
-        return features_X, features_Y
-
-    # more details on cross fold
-    #https://gist.github.com/zacstewart/5978000
-    def kfoldClassification(self, X, y, classifier):
-        # Let's do a 2-fold cross-validation of the SVC estimator
-        print 'Cross validation ', cross_val_score(classifier, X, y, cv=10, n_jobs=2)
 
 
     def KFOLDTEST(self, text, sent):
         k_fold = KFold(n=len(text), n_folds=6)
 
-
-
-
         pipeline = Pipeline([
-        ('vectorizer', CountVectorizer(ngram_range=(1, 2), tokenizer=self.tokenize_data)),
-        ('tfidf', TfidfTransformer(norm="l2", smooth_idf=True, use_idf=True)),
-        ('classifier', OneVsRestClassifier(LinearSVC())
-        )])
-
+            ('vectorizer', CountVectorizer(ngram_range=(1, 2), tokenizer=self.tokenize_data)),
+            ('tfidf', TfidfTransformer(norm="l2", smooth_idf=True, use_idf=True)),
+            ('classifier', OneVsOneClassifier(LinearSVC())
+            )])
 
         scores = []
         for train_indices, test_indices in k_fold:
-            #print('Train: %s | test: %s' % (train_indices, test_indices))
+            # print('Train: %s | test: %s' % (train_indices, test_indices))
             train_text = text[train_indices]
             train_y = sent[train_indices]
 
             test_text = text[test_indices]
             test_y = sent[test_indices]
-
 
             pipeline.fit(train_text, train_y)
             score = pipeline.score(test_text, test_y)
@@ -243,24 +157,40 @@ class DataReader(object):
 
 
     def tryanother(self, trainingdata):
-        _all_values = []
-        _all_sentiments = []
+        aDict = {}
         for value in trainingdata:
             phrase = value.Phrase
 
             if phrase.startswith(tuple(self.matchingparam)):
                 phrase = phrase[1:]
             if phrase.endswith(tuple(self.matchingparam)):
-                phrase = phrase[0:len(phrase)-1]
+                phrase = phrase[0:len(phrase) - 1]
 
             phrase = phrase.strip()
-            _all_values.append(phrase)
-            _all_sentiments.append(value.Sentiment)
 
+            aDict[phrase] = value.Sentiment
 
+            '''
+            if not aDict.has_key(phrase):
+                aDict[phrase] = value.Sentiment
+            elif not aDict[phrase] == value.Sentiment:
+                print(value.PhraseId)
+            '''
+
+        _all_values = aDict.keys()
+        _all_sentiments = aDict.values()
+
+        #self.KFOLDTEST(np.asarray(_all_values), np.asarray(_all_sentiments))
 
         count_vectorizer = CountVectorizer(ngram_range=(1, 2), tokenizer=self.tokenize_data)
         count = count_vectorizer.fit_transform(_all_values)
+        '''
+        word_freq_df = pd.DataFrame({'term': count_vectorizer.get_feature_names(),
+                                     'occurrences': np.asarray(count.sum(axis=0)).ravel().tolist()})
+        word_freq_df['frequency'] = word_freq_df['occurrences'] / np.sum(word_freq_df['occurrences'])
+        print word_freq_df.sort('occurrences', ascending=False).head(20)
+        '''
+
 
         tfidf = TfidfTransformer(norm="l2", smooth_idf=True, use_idf=True)
         data = tfidf.fit_transform(count)
@@ -269,9 +199,6 @@ class DataReader(object):
         classfier.fit(data, _all_sentiments)
 
 
-        #self.KFOLDTEST(np.asarray(_all_values), np.asarray(_all_sentiments))
-        #learn pipeline
-        #http://zacstewart.com/2014/08/05/pipelines-of-featureunions-of-pipelines.html
 
 
         test_X = []
@@ -283,9 +210,13 @@ class DataReader(object):
         example_counts = count_vectorizer.transform(test_X)
         tfidf1 = tfidf.transform(example_counts)
         predicted = classfier.predict(tfidf1)
-        #print(predicted)
+
+        # print(predicted)
 
 
+        # Data to write the content into the CSV , for getting this comment the above to take entire training set
+        #as the real data
+        #along with that call the method @getRealData
         '''
         tutorial_out = open('sentiment.csv', 'wb')
         mywriter = csv.writer(tutorial_out)
@@ -300,14 +231,13 @@ class DataReader(object):
              mywriter.writerow(item)
 
         tutorial_out.close()
-
         '''
+
 
         test_X = []
         sent_test = []
         phraseid_test = []
         for line in test_data:
-
             test_X.append(line.Phrase)
             sent_test.append(line.Sentiment)
             phraseid_test.append(line.PhraseId)
@@ -323,59 +253,11 @@ class DataReader(object):
             if prediction == sent_test[counter]:
                 count += 1
             else:
-                error.append((phraseid_test[counter],[prediction,sent_test[counter]]))
-
+                error.append((phraseid_test[counter], [prediction, sent_test[counter]]))
 
             counter += 1
 
-        print(' Accuracy1 ', count, 'test1 ', len(test_data),' Percentage ',float(count)/float(len(test_data)))
-        print(error)
-
-
-    # http://scikit-learn.org/stable/modules/feature_extraction.html#text-feature-extraction
-
-
-    def classifier(self, features_X, features_Y, test_data, all_words):
-        all_words = list(set(all_words))
-
-        classifier = Pipeline([
-            ('vectorizer', CountVectorizer(ngram_range=(1, 2), stop_words='english')),
-            ('tfidf', TfidfTransformer(norm="l2", smooth_idf=True, use_idf=True)),
-            ('classifier', OneVsRestClassifier(LinearSVC())
-            )])
-
-
-
-        #OneVsRestClassifier(LinearSVC()) - 92.3
-        #MultinomialNB(alpha=1.0,class_prior=None,fit_prior=True) - 86.7
-        #generally sentiment analysis works better with BernoulliNB because of boolean nature
-        #BernoulliNB(alpha=1.0, binarize=0.0, class_prior=None, fit_prior=True) - 84.5
-        #svm.SVC(kernel='linear', cache_size=5000) - 92.3
-
-        size = len(features_X)
-        X_train = np.array(features_X[0:size])
-        y_train = np.array(features_Y[0:size])
-
-        #print(' feaxtureX ',X_train[1:4],' length ',len(X_train), ' Y ',y_train[58000:58010])
-        self.kfoldClassification(features_X, features_Y, classifier)
-        classifier = classifier.fit(X_train, y_train)
-        count = 0
-
-        error = []
-        for line in test_data:
-            phrase = line.Phrase
-            sentiment = line.Sentiment
-            PhraseId = line.PhraseId
-            predicted = classifier.predict([phrase])
-            #print(' predicted ',predicted[0],' Sent ',sentiment)
-            if predicted == self.sents[sentiment]:
-                count += 1
-            else:
-                error.append(PhraseId)
-
-        print(' Accuracy ', count, 'test ', len(test_data))
-        print('error ', len(error))
-        error.sort()
+        print(' Accuracy1 ', count, 'test1 ', len(test_data), ' Percentage ', float(count) / float(len(test_data)))
         print(error)
 
 
@@ -390,9 +272,3 @@ if __name__ == '__main__':
     reader = DataReader()
     train_data, test_data = reader.getTrainTestData()
     reader.tryanother(train_data)
-
-    '''
-    specific_words_dict, all_words, reader.most_significant_words = reader.wordFrequency(list(train_data))
-    features_X, features_Y = reader.feature_extraction(specific_words_dict)
-    reader.classifier(features_X, features_Y, test_data,list(train_data))
-    '''
