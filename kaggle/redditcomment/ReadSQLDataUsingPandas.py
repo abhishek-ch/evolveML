157d05096b1de5e006ae2c6dc6b8b4a477b8fa27
Abhishek Choudhary <abhishek.create@gmail.com>
2015-08-17 10:25:03 +0100

    reddit

--- kaggle/redditcomment/ReadSQLDataUsingPandas.py
+++ kaggle/redditcomment/ReadSQLDataUsingPandas.py
@@ -1,1 +1,21 @@
 __author__ = 'achoudhary'
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+import pylab
+
+fig = plt.figure()
+
+db = '../input/database.sqlite'
+conn = sqlite3.connect(db)
+cur = conn.cursor()
+
+df = pd.read_sql(('SELECT * FROM May2015 \
+WHERE rowid % 6000 = 0 and score<1000 \
+ORDER BY created_utc'),conn)
+
+print(df.axes[1])
+
+plt.plot(df.created_utc, df.score)
+
+fig.savefig('graph.png')
