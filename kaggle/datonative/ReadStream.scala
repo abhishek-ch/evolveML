b552180e5d7a1ad5da285670314cae871b3b69c3
Abhishek Choudhary <abhishek.create@gmail.com>
2015-08-22 22:20:22 +0100

    streaming with scala

--- kaggle/datonative/ReadStream.scala
+++ kaggle/datonative/ReadStream.scala
@@ -1,28 +1,36 @@
 /**
- * Created by abc on 20/08/2015.
+ * Created by abc on 22/08/2015.
  */
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
 
-
-import org.apache.spark._
-
-object Read{
+object ReadStream {
   def main(args: Array[String]) {
-    val sparkConf = new SparkConf().setAppName("Reading Zip & Contents").setMaster("local[2]")
+    val sparkConf = new SparkConf().setAppName("Reading Zip & Contents")
     val sc = new SparkContext(sparkConf)
 
-    val readZip = sc.wholeTextFile("/Volumes/work/data/kaggle/dato/test/5.zip")
+    val readZip = sc.wholeTextFiles("/Volumes/work/data/kaggle/dato/test/5.zip").cache()
 
     val contents = readZip.flatMap({
       case (name , contents) => unzip(contents)
     })
 
+    val names = readZip.flatMap({
+      case (name , contents) => unzip(name)
+    })
+
+
+
+
+
+    contents.take(5)
     sc.stop()
   }
 
 
   def unzip(contents : String) : String = {
     println("=> "+contents)
-    return ""
+    return contents
   }
 }
-
