3c61ce4a4440a758899d05dba51658d5f1e04d85
Abhishek Choudhary <abhishek.create@gmail.com>
2015-09-06 20:44:00 +0100

    server

--- test/Server.py
+++ test/Server.py
@@ -1,1 +1,13 @@
 __author__ = 'abc'
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('127.0.0.1', 52300))
+serversocket.listen(5) # become a server socket, maximum 5 connections
+
+while True:
+    connection, address = serversocket.accept()
+    buf = connection.recv(64)
+    if len(buf) > 0:
+        print buf
+        break
