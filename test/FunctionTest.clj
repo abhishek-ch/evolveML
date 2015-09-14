67a38816c0f4a768008b3c5794059d3a6d5aee37
ABHISHEK CHOUDHARY <abhishek.create@gmail.com>
2015-09-14 17:25:36 +0100

    take-while drop-while clojure func

--- test/FunctionTest.clj
+++ test/FunctionTest.clj
@@ -1,4 +1,7 @@
  (ns FunctionTest)
+
+ ;;reference http://www.braveclojure.com/core-functions-in-depth/
+
 (println "Test Function")
 
 (defn testMap
@@ -31,3 +34,34 @@
 
 (println "map as a function 1 " (stats [1 2 3 4 5]))
 (println "map as a function 2 " (stats [10 3 40]))
+
+
+(println "REDUCE " (reduce (fn
+                             [map-value [key value]]
+                             (if (> value 4)
+                               (assoc map-value key value)
+                               map-value))
+                           {}
+                           {:A 9 :B 10 :c 3.9 :d 4.1 :e 2.2 :f 8.9 :g 4}
+                           ))
+
+ ;;test take-while
+
+(def food-journal
+  [{:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}
+   {:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   ])
+
+
+(println "take-while test " (take-while #(> (:month %) 3) food-journal) )
+
+(println "take-while & drop-while " (take-while #(= (:month %) 1)  (drop-while #(> (:month %) 3) food-journal)  ) )
+
+(take-while #(< (:month %) 4)
+            (drop-while #(< (:month %) 2) food-journal))
